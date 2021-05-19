746a292b427da8c140bd176d4d043eb4fa06a2c6
Merryn <merrynhr@gmail.com>
2021-05-19 10:29:19 +0100

    Refactoring with string interpolation

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+# first, we puts the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,7 +13,6 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-# finally, we print the total number of students
-print "Overall, we have "
-print 11
-print " great students"
+# finally, we puts the total number of students
+puts "Overall, we have #{student_count} great students"
+
