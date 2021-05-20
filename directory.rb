7acecd392ba9354063c65b63a66453238d74fe3b
Merryn <merrynhr@gmail.com>
2021-05-20 17:25:04 +0100

    Allows for cases in which there is only 1 student

--- directory.rb
+++ directory.rb
@@ -31,7 +31,11 @@ def print_header
   end
   
   def print_footer(students)
-   puts "Overall, we have #{students.count} great students"
+    if students.count == 1
+      puts "Overall, we have 1 great student"
+    else
+      puts "Overall, we have #{students.count} great students"
+    end
   end
    
   def input_students
